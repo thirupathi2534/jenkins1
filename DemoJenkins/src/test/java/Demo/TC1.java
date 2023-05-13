87ae4ab481b041c920bcda0428bd883b56c50d40
thiru <thiru@DESKTOP-47BH4VF>
2023-05-13 15:27:43 +0530

    first jenkins build

---
+++ DemoJenkins/src/test/java/Demo/TC1.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC1 {
+	
+	@Test
+	public void m1()
+	{
+		Reporter.log("m1 pass");
+	}
+
+}
---
+++ DemoJenkins/src/test/java/Demo/TC2.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC2 {
+	
+	@Test
+	public void m2()
+	{
+		Reporter.log("m2 pass");
+	}
+
+}
